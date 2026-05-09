7b40a1909d0fb211be51c7aa14bfd22ed76f28b7
Eduardo B. <bonomettieduardo2@gmail.com>
2026-05-08 22:29:34 -0300

    2

--- app/app.py
+++ app/app.py
@@ -1,1 +1,38 @@
+from google import genai
 import streamlit as st
+import re
+
+def app(tech: str, api_key: str) -> str:
+
+    prompt = f'''Generate only GitHub-style Markdown badges using Shields.io.
+    Rules:
+    Output ONLY badges (no explanations, no text, no titles)
+    Each item must follow this format: 
+    Return only valid Markdown lines
+    Do not include code blocks
+    Do not include comments or extra formatting
+    Use correct official logos from Simple Icons
+    Choose appropriate colors for each technology
+    If unsure about color, pick a standard official brand color
+    Technologies will be provided by the user
+    Input:
+    {tech}
+    Output:
+    ONLY badges in Markdown format and ONLy test ordered in 'Input:
+    {tech}' '''
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+    return response.text
+
+    # for m in client.models.list():
+        # print(m.name)
+
+def is_valid_badge(text: str) -> bool:
+    pattern = r"^!\[.+\]\(https://img\.shields\.io/badge/.+\)$"
+    return bool(re.match(pattern.strip(), text.strip()))
+
+#print(app("GitHub"))
---
+++ app/ui.py
@@ -0,0 +1,27 @@
+from types import NoneType
+
+import streamlit as st
+from pydantic.v1 import NoneStr
+
+import app
+
+def main():
+    st.write(
+        """StackAI é uma ferramenta com inteligência artificial
+    que gera badges profissionais no estilo GitHub a partir
+    do nome de qualquer tecnologia, facilitando a criação
+    de READMEs e portfólios.""")
+
+    # Sua API key pegue em -------> https://aistudio.google.com/prompts/new_chat
+    # Coloque dentro do arquivo app/.streamlit/secrets.toml dessa maneira -------> GEMINI_API_KEY = 'sua api_key'
+    api_key = st.secrets["GEMINI_API_KEY"]
+    try:
+        text = app.app("postgresql", api_key)
+        print(text)
+    except NoneType as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    main()
+
